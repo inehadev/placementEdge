c46733837683bdd6a2804814a3d15f7815b6aff2
inehadev <156919946+inehadev@users.noreply.github.com>
2024-08-04 22:15:05 +0530

    header

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,48 +1,126 @@
-
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Checkbox } from '@/components/ui/checkbox'
 import { Button } from '@/components/ui/button'
+import axios from 'axios'
+import { z } from "zod"
+import { signupSchema } from '@/app/schema/signupSchema'
+import { FormField, FormItem, FormLabel, FormControl, Form } from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useToast } from '@/components/ui/use-toast'
 
 const page = () => {
+
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+      email: ""
+    }
+
+
+  })
+
+  const onSubmit = async (data: z.infer<typeof signupSchema>) => {
+    try {
+      const response = await axios.post('/api/sign-up', data)
+      toast({
+        title: 'Success',
+        description: response.data.message
+      })
+
+
+
+    } catch (error) {
+      console.error("signup errror", error)
+      toast({
+        title: 'False',
+        description: "SignUp Failed"
+      })
+
+    }
+
+  }
   return (
     <>
-    <div className=' flex justify-start bg-blue-600  h-screen w-full gap-32 items-center  '>
-     <div className=' '>
-        <img src="sign-up.png" alt="" className='h-screen  ml-10 mt-6 w-full' />
-     </div>
-     <div className='justify-center items-center '>
-   <h1 className='text-4xl text-white'>Sign-Up</h1>
-   <p className='text-white opacity-70 mt-1'>Enroll here! to start your placement journey</p>
+      <div className=' flex justify-start bg-blue-600  h-screen w-full gap-32 items-center  '>
+        <div className=' '>
+          <img src="sign-up.png" alt="" className='h-screen  ml-10 mt-6 w-full' />
+        </div>
+        <div className='justify-center items-center '>
+          <h1 className='text-4xl text-white'>Sign-Up</h1>
+          <p className='text-white opacity-70 mt-1'>Enroll here! to start your placement journey</p>
 
-   <div className='mt-2'>
-   <Label htmlFor="username" className='text-white '>Username</Label>
-   <Input type="text" id="email" placeholder="username" />
-   </div>
-
-   <div className='mt-2'>
-   <Label htmlFor="email" className='text-white '>Email</Label>
-   <Input type="email" id="email" placeholder="Email" />
-   </div>
-   
-
-   <div className='mt-2'>
-   <Label htmlFor="password" className='text-white '>Password</Label>
-   <Input type="Pssword" id="email" placeholder="Password" />
-   </div>
-
-   <div className="flex items-center space-x-2  mt-1 gap-2  text-gray"> <Checkbox id="terms2"  /> Accept terms and conditions </div>
-
-   <Button className='w-full mt-3 '>SignUp</Button>
-
-   <p className='mt-2'>Already have an account? <a href='/sign-in'>SignIn</a></p>
+          <Form {...form} >
+            <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-1 mt-2 '>
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="john" {...field} />
+                    </FormControl>
 
 
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="john@gmail.com" {...field} />
+                    </FormControl>
 
 
-     </div>
-    </div>
+                  </FormItem>
+                )}
+              />
+
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="" {...field} />
+                    </FormControl>
+
+
+                  </FormItem>
+                )}
+
+              />
+
+              <Button className='w-full mt-3 ' type='submit'>SignUp</Button>
+
+              <p className='mt-2'>Already have an account? <a href='/sign-in'>SignIn</a></p>
+
+            </form>
+          </Form>
+
+
+
+
+
+
+        </div>
+      </div>
+
     </>
   )
 }
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,14 +1,19 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <>
-    <div className='border border-1px solid to-blue-500 flex '>
-     <div>PlacementEdge</div>
-     <div className='bg-white'>
+    <header className='flex justify-evenly sticky items-center h-[100px] bg-blue-600 text-white '>
+      <div className='mr-12'><h1 className='  text-3xl font-normal font-style: italic text-center'>PlacementEdge</h1></div>
+      <div className=' '>
+        <Link className='ml-12 text-xl' href='/about'>About</Link>
+        <Link className='ml-12 text-xl' href='/sign-up'>SignUp</Link>
 
-     </div>
-    </div>
+        
+      </div>
+    </header>
     </>
   )
 }
