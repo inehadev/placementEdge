69b5327e5ce016e848e2053d7eb90ad3d2ba6d54
inehadev <156919946+inehadev@users.noreply.github.com>
2024-08-04 23:00:23 +0530

    error

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
     <>
     <div className=' flex justify-start bg-blue-600  h-screen w-full gap-32 items-center  '>
      <div className=' '>
-     <Image src="/sign-up.png" alt="" className="ml-10 mt-6 w-full"  height={100} width={400} />
+     <Image src="/sign-up.png" alt="" className="ml-10 mt-6 "  height={100} width={400} />
      </div>
      <div className='justify-center items-center '>
    <h1 className='text-4xl text-white'>Sign-In</h1>
@@ -27,13 +27,13 @@ const Page = () => {
 
    <div className='mt-2'>
    <Label htmlFor="password" className='text-white '>Password</Label>
-   <Input type="Pssword" id="email" placeholder="Password" />
+   <Input type="password" id="email" placeholder="Password" />
    </div>
 
 
    <Button className='w-full mt-5 '>SignIn</Button>
 
-   <p className='mt-2'>Don't have an account? <a href='/sign-up'>SignUp</a></p>
+   <p className='mt-2'>   Don't have an account? <a href='/sign-up'>SignUp</a> </p>
 
 
 
