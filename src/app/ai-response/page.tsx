49dc0982b3631890b4534a94d2ac572f4796644a
inehadev <156919946+inehadev@users.noreply.github.com>
2024-08-18 16:24:55 +0530

    pdf to data extraction

--- src/app/ai-response/page.tsx
+++ src/app/ai-response/page.tsx
@@ -18,8 +18,7 @@ const Page = () => {
     try {
       setloading(true);
       const response = await axios({
-        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.NEXT_PUBLIC_GEMINI_API}`, // Ensure to use NEXT_PUBLIC_ for exposing API keys to the client side
-        method: "POST",
+        url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${process.env.NEXT_PUBLIC_GEMINI_API}`, 
         data: {
           contents: [{ parts: [{ text: question }] }]
         }
@@ -56,7 +55,7 @@ const Page = () => {
         <div className="m-5 font-normal"><pre className="text-center items-center mb-24">{answer}</pre></div>
 
 
-        <div className="fixed bottom-0 left-0 right-0 mx-20 mb-5 flex ">
+        <div className="fixed bottom-0 left-0 right-0 mx-48 mb-5 flex ">
           <textarea className="w-full border border-green-950 rounded-md  px-5 py-1 " value={question} placeholder="ask your placement doubt..?" onChange={(e) => { setQuestion(e.target.value) }} />
           <Button type="submit" className="bg-green-950 text-white ml-3 mt-2" onClick={generateAnswer}>Get answer</Button></div>
 
--- src/app/api/apptitude/route.ts
+++ src/app/api/apptitude/route.ts
@@ -1,111 +1,41 @@
-import puppeteer from 'puppeteer';
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+import fs from 'fs';
+import path from 'path';
 
-export async function POST(request: Request) {
-
-    try {
-        const browser = await puppeteer.launch({
-            headless: true
-        })
-
-        const page = await browser.newPage()
-
-        await page.goto('https://www.geeksforgeeks.org/aptitude-questions-and-answers/', {
-           waitUntil: 'networkidle2'
-        })
-        
-        // await page.waitForSelector('.QuizQuestionCard_quizCard__9T_0J', { timeout: 5000 });
-         // Wait for the blockquote to be present on the page
-         await page.waitForSelector('blockquote', { timeout: 15000 });
-
-         const content = await page.evaluate(() => {
-             const blockquote = document.querySelector('blockquote');
-             if (blockquote) {
-                 return {
-                     text: blockquote.querySelector('p')?.textContent?.trim(),
-                     list: Array.from(blockquote.querySelectorAll('ol li')).map(item => {
-                         const link = item.querySelector('a');
-                         return {
-                             text: link ? link.textContent?.trim() : item.textContent?.trim(),
-                             url: link ? link.href : null
-                         };
-                     })
-                 };
-             } else {
-                 return { text: 'No content found', list: [] };
-             }
-         });
-        await browser.close()
-
-        return new Response(JSON.stringify(content), {
-            status: 200,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        } catch (error) {
-
-            console.error('Error during web scraping:', error);
-
-            return new Response(JSON.stringify({ error: 'Failed to scrape data' }), {
-                status: 500,
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-
-        }
-    }
+const apiKey = process.env.PDF_API; 
 
 
-    
-      
+export default async function handler(request:Request) {
+  const pdfPath = path.join(process.cwd(), 'public', 'example.pdf');
+  
+  try {
+ 
+    const fileData = fs.readFileSync(pdfPath);
+    const base64Pdf = Buffer.from(fileData).toString('base64');
 
+   
+    const response = await axios.post(
+      'https://api.pdf.co/v1/pdf/convert/to/json',
+      {
+        file: base64Pdf,
+        pages: '1-', 
+      },
+      {
+        headers: {
+          'x-api-key': apiKey,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
-
-// import puppeteer from 'puppeteer';
-// import fs from 'fs';
-
-// export async function GET(request: Request) {
-//     try {
-//         const browser = await puppeteer.launch({
-//             headless: true
-//         });
-
-//         const page = await browser.newPage();
-
-//         // Go to the target page
-//         await page.goto('https://www.geeksforgeeks.org/quizzes/numbers-gq/', {
-//             waitUntil: 'networkidle0', // or 'domcontentloaded'
-//             timeout: 120000 // 120 seconds // Wait until the network is idle
-//         });
-
-//         // Take a screenshot of the full page
-//         const screenshotBuffer = await page.screenshot({ fullPage: true });
-
-//         // Close the browser
-//         await browser.close();
-
-//         // Convert screenshot to base64
-//         const base64Image = Buffer.from(screenshotBuffer).toString('base64');
-//         const dataUri = `data:image/png;base64,${base64Image}`;
-
-//         return new Response(JSON.stringify({ image: dataUri }), {
-//             status: 200,
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//         });
-
-
-//     } catch (error) {
-//         console.error('Error during screenshot capture:', error);
-
-//         return new Response(JSON.stringify({ error: 'Failed to capture screenshot' }), {
-//             status: 500,
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//         });
-//     }
-// }
+  return Response.json({
+    status:200,
+    message:"pdf data is sucessfully extracted ",
+    response
+  })
+  } catch (error) {
+    console.error('Error extracting PDF data:', error);
+ 
+  }
+}
