53a94e9bec6239a59cbec30f306562c4e38057e0
inehadev <156919946+inehadev@users.noreply.github.com>
2024-09-03 21:42:54 +0530

    some modification

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -5,25 +5,25 @@ const Footer = () => {
     <div className='ml-10 mr-10 h-[200px] bg-opacity-45 bg-green-200 flex justify-evenly gap-24'>
   <div className='text-xl ml-10 justify-center items-center flex'>PlacementEdge</div>
   <div className='flex flex-col justify-center items-center gap-1'>
+     <span>Product</span> 
      <span>Overview</span> 
-     <span>Overview</span> 
-     <span>Overview</span> 
-     <span>Overview</span> 
-     <span>Overview</span> 
+     <span>Features</span> 
+     <span>Solution</span> 
+     <span>Tutorial</span> 
   </div>
   <div className='flex flex-col justify-center items-center gap-1'>
-  <span>Overview</span> 
-     <span>Overview</span> 
-     <span>Overview</span> 
-     <span>Overview</span> 
-     <span>Overview</span> 
+  <span>Company</span> 
+     <span>Carreers</span> 
+     <span>Press</span> 
+     <span>News</span> 
+     <span>Mediakit</span> 
   </div>
   <div className='flex flex-col justify-center items-center gap-1'>
-  <span>Overview</span> 
-     <span>Overview</span> 
-     <span>Overview</span> 
-     <span>Overview</span> 
-     <span>Overview</span> 
+  <span></span> 
+     <span></span> 
+     <span></span> 
+     <span></span> 
+     <span></span> 
   </div>
     </div>
   )
