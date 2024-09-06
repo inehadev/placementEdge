f4e23a2c511d43f7928372d45be04bf5c5da143b
inehadev <156919946+inehadev@users.noreply.github.com>
2024-09-06 21:39:25 +0530

    modification iwith footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,29 +2,23 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='ml-10 mr-10 h-[200px] bg-opacity-45 bg-green-200 flex justify-evenly gap-24'>
-  <div className='text-xl ml-10 justify-center items-center flex'>PlacementEdge</div>
+    <div className=' h-[200px]  bg-green-900 flex justify-evenly gap-40'>
+  <div className='text-xl ml-24 justify-center opacity-80 items-center flex'><span className='text-2xl opacity-70'>PlacementEdge</span></div>
   <div className='flex flex-col justify-center items-center gap-1'>
-     <span>Product</span> 
-     <span>Overview</span> 
-     <span>Features</span> 
-     <span>Solution</span> 
-     <span>Tutorial</span> 
+     <span className='opacity-70'>Product</span> 
+     <span className='opacity-70'>Overview</span> 
+     <span className='opacity-70'>Features</span> 
+     <span className='opacity-70'>Solution</span> 
+     <span className='opacity-70'>Tutorial</span> 
   </div>
   <div className='flex flex-col justify-center items-center gap-1'>
-  <span>Company</span> 
-     <span>Carreers</span> 
-     <span>Press</span> 
-     <span>News</span> 
-     <span>Mediakit</span> 
+  <span className='opacity-70'>Company</span> 
+     <span className='opacity-70'>Carreers</span> 
+     <span className='opacity-70'>Press</span> 
+     <span className='opacity-70'>News</span> 
+     <span className='opacity-70'>Mediakit</span> 
   </div>
-  <div className='flex flex-col justify-center items-center gap-1'>
-  <span></span> 
-     <span></span> 
-     <span></span> 
-     <span></span> 
-     <span></span> 
-  </div>
+  
     </div>
   )
 }
