66b33a9cee1833b07c93bb1ef4088e816865728e
inehadev <156919946+inehadev@users.noreply.github.com>
2024-09-07 12:19:16 +0530

    responsive

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { Button } from '@/components/ui/button'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
@@ -10,11 +9,11 @@ const Navbar = () => {
   return (
     <>
     <header className='flex justify-evenly sticky items-center bg-green-900 h-[70px]  text-white '>
-      <div className='mr-32'><h1 className='  text-3xl font-normal font-style: italic text-center'>PlacementEdge</h1></div>
-      <div className=' flex ml-32'>
-      <Link className='ml-24 text-xl' href='/about'>About</Link>
+      <div className='mr-32  sm:mr-2'><h1 className='  text-3xl font-normal font-style: italic text-center'>PlacementEdge</h1></div>
+      <div className=' flex ml-32 md:ml-24 sm:ml-10'>
+      <Link className='base:ml-24 md:ml-20 text-xl' href='/about'>About</Link>
         {!session ? (
-          <Link className='ml-12 text-xl' href='/sign-up'>Sign Up</Link>
+          <Link className='base:ml-12 sm:ml-5 text-xl' href='/sign-up'>Sign Up</Link>
         ) : (
           <div className='ml-12 text-xl flex'>
             <span>Welcome, {session.user?.name || 'User'}!</span>
